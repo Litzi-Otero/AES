4489f1c99417392cf11e57d8b0f4d76bcf7336c5
Litzi-Otero <2022371132@uteq.edu.mx>
2025-10-07 23:45:55 -0600

    add test

--- test/index.test.js
+++ test/index.test.js
@@ -2,6 +2,6 @@ const hash = require('./testindex');
 
 test('Verificar Hash', () => {
     const message = 'contraseña';
-    const hash = hash(message);
-    expect(hash).toEqual();
+    const hashtest = hash(message);
+    expect(hashtest).toEqual("edf9cf90718610ee7de53c0dcc250739239044de9ba115bb0ca6026c3e4958a5");
 });
